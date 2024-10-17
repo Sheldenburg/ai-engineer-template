e75ddc94c5fbd24ab971d9bb3242bcd4b49620a1
Sheldenburg <sheldoncn@live.com>
2024-10-18 00:33:25 +1100

    fix the initial data load in prod

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -1,21 +1,16 @@
 #! /usr/bin/env bash
 
-cd lambda/app
+cd src
 # Load environment variables
 export $(grep -v '^#' .env | xargs)
 
-
 # Let the DB start
-# python /app/app/backend_pre_start.py
-# python backend_pre_start.py
+python backend_pre_start.py
 
+cd app
 # Run migrations
 alembic upgrade head
 
 cd ..
-
 # Create initial data in DB
-# python /app/app/initial_data.py
 python initial_data.py
-
-cd ..
